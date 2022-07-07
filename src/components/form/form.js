0d97bdc6916b0ee2e48047e41c5da2d16d0b104d
Kelsey H <kels-hall23@hotmail.com>
2022-07-07 14:36:11 -0400

    preview color updating

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,28 +1,56 @@
+import { useState } from 'react';
+
 const inputFormStyle = {
   input: `block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`,
   label: `absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1`,
 };
 const buttonStyle =
-  'bg-button-dark hover:bg-button-hover text-text-light rounded-full py-1.5 px-4 ';
+  'bg-button-dark hover:bg-button-hover text-text-light rounded-full py-1.5 px-4 my-4 ';
 
 const ColorForm = () => {
+  const [HexCode, setHexCode] = useState('');
+
   return (
-    <form className="flex-col my-4 mx-auto w-3/4">
-      <div className="relative">
-        <input type="text" className={inputFormStyle.input} placeholder=" " />
-        <label className={inputFormStyle.label}>Search Colour</label>
-      </div>
-      <input type="color" />
-      <fieldset>
-        <div className="w-full h-20 border-2">preview</div>
-        <div className="relative">
-          <input type="text" className={inputFormStyle.input} placeholder=" " />
-          <label className={inputFormStyle.label}>reference name</label>
+    <form
+      className="flex-col my-4 mx-auto w-10/12"
+      onSubmit={(e) => {
+        e.preventDefault();
+        return console.log(e.target.hexCodeInput.value);
+      }}
+    >
+      <div className="flex">
+        <div className="relative  w-10/12">
+          <input
+            type="text"
+            name="hexCodeInput"
+            maxLength="6"
+            onChange={(e) => setHexCode(e.target.value)}
+            className={inputFormStyle.input}
+            placeholder=" "
+          />
+          <label className={inputFormStyle.label}>Search Colour</label>
         </div>
-        <button type="submit" className={buttonStyle}>
-          add to scheme
-        </button>
-      </fieldset>
+        <input type="color" className="w-2/12" value={'#' + HexCode} />
+      </div>
+
+      <div
+        className="w-full h-20 border-2 relative"
+        style={{ backgroundColor: '#' + HexCode }}
+      >
+        <p className="absolute bottom-0 pl-1.5 text-Primary-light">preview</p>
+      </div>
+      <div className="relative">
+        <input
+          type="text"
+          name="referenceNameInput"
+          className={inputFormStyle.input}
+          placeholder=" "
+        />
+        <label className={inputFormStyle.label}>reference name</label>
+      </div>
+      <button type="submit" className={`${buttonStyle}`}>
+        add to scheme
+      </button>
     </form>
   );
 };
