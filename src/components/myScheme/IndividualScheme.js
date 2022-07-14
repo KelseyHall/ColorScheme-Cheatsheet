f70a986158cf2dcd9ee0d53c1ccb97932e87db20
Kelsey H <kels-hall23@hotmail.com>
2022-07-14 11:57:30 -0400

    toggle rgb and hex

--- src/components/myScheme/IndividualScheme.js
+++ src/components/myScheme/IndividualScheme.js
@@ -14,81 +14,85 @@ function hexToRgb(hex) {
     var r = parseInt(result[1], 16);
     var g = parseInt(result[2], 16);
     var b = parseInt(result[3], 16);
-    return r + ',' + g + ',' + b; //return 23,14,45 -> reformat if needed
+    return r + ',' + g + ',' + b;
   }
   return null;
 }
-// const RGBToHSL = (r, g, b) => {
-//   r /= 255;
-//   g /= 255;
-//   b /= 255;
-//   const l = Math.max(r, g, b);
-//   const s = l - Math.min(r, g, b);
-//   const h = s
-//     ? l === r
-//       ? (g - b) / s
-//       : l === g
-//       ? 2 + (b - r) / s
-//       : 4 + (r - g) / s
-//     : 0;
-//   return [
-//     60 * h < 0 ? 60 * h + 360 : 60 * h,
-//     100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
-//     (100 * (2 * l - s)) / 2,
-//   ];
-// };
 
 const IndividualScheme = ({ data, setData }) => {
   const [showDetails, setShowDetails] = useState(true);
+  const [convertCode, setConvertCode] = useState('hexcode');
+
   let groupedData = groupScheme(data, 'schemeName');
 
   const schemeName = useLocation().search;
   //removes % for spaces, takes out ? from url
   const schemeNameConvert = schemeName.replaceAll('%', ' ').substring(1);
 
+  let displayCovertedCode = (code) => {
+    if (convertCode === 'rgb') {
+      return hexToRgb(code);
+    }
+    return code;
+  };
+
   return (
-    <div>
-      <h2 className="h2">Scheme {schemeNameConvert}</h2>
+    <div className="max-w-screen-md m-auto">
+      <h2 className="">Scheme {schemeNameConvert}</h2>
 
-      <label>Display:</label>
-      <select
-        id="displayValue"
-        name="displayValue"
-        onChange={(e) => console.log(e.target.value)}
+      <form className="float-left">
+        <label>Display:</label>
+        <select
+          id="displayValue"
+          name="displayValue"
+          onChange={(e) => setConvertCode(e.target.value)}
+        >
+          <option value="hexcode">Hex Code</option>
+          <option value="rgb">RGB</option>
+        </select>
+      </form>
+
+      <button
+        className="float-right"
+        onClick={() => setShowDetails(!showDetails)}
       >
-        <option value="hexcode">Hex Code</option>
-        <option value="hsl">HSL</option>
-        {/*<option value="rgb">RGB</option>*/}
-      </select>
-
-      <button onClick={() => setShowDetails(!showDetails)}>
         {showDetails ? 'Show Details' : 'Hide Details'}
       </button>
-      <div className="h-20 w-8/12">
-        {groupedData[schemeNameConvert].map((each) => (
-          <div key={each.id} className="w-full h-full my-3 flex">
-            {console.log(each)}
-            {showDetails ? null : (
-              <div className="flex">
-                <h4 className="m-1 block">{each.referenceNameInput}</h4>
-                <p className="m-1">HEX {each.textColorInput}</p>
-                {/*<p className="m-1">
-                  HSL {RGBToHSL(hexToRgb(each.textColorInput))}
-            </p>*/}
-                <p className="m-1">RGB {hexToRgb(each.textColorInput)}</p>
+      <div className="h-20 w-full">
+        {groupedData[schemeNameConvert].map((each) => {
+          return (
+            <div key={each.id} className="w-full h-full my-3 flex">
+              {showDetails ? null : (
+                <div className="flex-col w-2/6 font-bold">
+                  <h4 className="m-1 w-full block">
+                    {each.referenceNameInput}
+                  </h4>
+                  <p className="m-1">
+                    <span className="text-Primary-light">HEX</span>{' '}
+                    {each.textColorInput}
+                  </p>
+                  <p className="m-1">
+                    <span className="text-Primary-light">RGB</span>{' '}
+                    {hexToRgb(each.textColorInput)}
+                  </p>
+                </div>
+              )}
+              <div
+                style={{ backgroundColor: each.textColorInput }}
+                className="w-full h-full my-3 p-1  "
+              >
+                {showDetails ? (
+                  <p className="bg-white w-fit p-1 font-bold">
+                    {displayCovertedCode(each.textColorInput)}
+                  </p>
+                ) : null}
               </div>
-            )}
-            <div
-              style={{ backgroundColor: each.textColorInput }}
-              className="w-full h-full my-3 "
-            >
-              {showDetails ? each.textColorInput : null}
+              <button onClick={() => handleDelete(each.id, data, setData)}>
+                X
+              </button>
             </div>
-            <button onClick={() => handleDelete(each.id, data, setData)}>
-              X
-            </button>
-          </div>
-        ))}
+          );
+        })}
         <PopupDisplay data={data} setData={setData} />
       </div>
     </div>
